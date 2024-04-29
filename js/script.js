232bbf39ff6e51de2a5c0a59e726ce1ea570a761
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-04-29 18:09:02 +0000

    commit #1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kenneth
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function findAgeClicked() {
+  const ageInput = parseInt(document.getElementById("age-entered").value)
+
+  if (ageInput >= 17) {
+    document.getElementById("answer").innerHTML = "You can watch R rated movies alone."
+  } else if (ageInput >= 13) {
+    document.getElementById("answer").innerHTML = "You can watch PG-13 rated movies alone."
+  } else if (ageInput >= 5) {
+    document.getElementById("answer").innerHTML = "You can watch G or PG rated movies alone."
+  } else {
+    document.getElementById("answer").innerHTML = "You are too young to watch movies alone."
+  }
+}
